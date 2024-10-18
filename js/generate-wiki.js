b19ab18dc8b28d60dd4c5fba8116c7389e951c62
RickyTheRacc <rickytheracc@gmail.com>
2024-10-18 17:04:16 -0500

    Format Generated HTML Files
    I'm actually quite proud of this actually :D

--- js/generate-wiki.js
+++ js/generate-wiki.js
@@ -5,6 +5,7 @@ const { JSDOM } = require('jsdom');
 const WIKI_DIRECTORY = '../wiki';
 const MD_DIRECTORY = '../md';
 const WIKI_ENTRY_FILE = '../html/index.html';
+const INDENT_SIZE = 4;
 
 const converter = new showdown.Converter({
     'noHeaderId': true,
@@ -40,15 +41,52 @@ function createWikiFileStructure(directory) {
 
 function parseMarkdown(file) {
     const markdown = fs.readFileSync(file).toString()
-    const html = converter.makeHtml(markdown);
+    const parsedHtml = converter.makeHtml(markdown);
 
     const entryTemplate = fs.readFileSync(WIKI_ENTRY_FILE).toString();
+
     const { window } = new JSDOM(entryTemplate);
     const document = window.document;
+    document.body.innerHTML = parsedHtml;
 
-    document.body.innerHTML = html;
+    return formatHtml(document.documentElement.outerHTML)
+}
 
-    return document.documentElement.outerHTML
+function formatHtml(html) {
+    let formatted = `<!DOCTYPE html>\n${html}`;
+
+    formatted = formatted.replace(/></g, '>\n<');
+    // We also remove whitespace from everything to add it properly
+    formatted = formatted.split('\n').map(line => line.trim()).join('\n');
+
+    let indentLevel = 0;
+
+    formatted = formatted.split('\n').map((line, index) => {
+        // First line is always the doctype tag, which isn't technically
+        // self-closing but is still a tag, so we ignore it manually
+        if (index === 0) return line;
+
+        const strippedLine = line.replaceAll(' ', '');
+
+        let isSelfClosing = strippedLine.match(/^<([a-z][a-z0-9]*)>.*<\/\1>$/) != null;
+        if (!isSelfClosing) isSelfClosing = strippedLine.match(/^<meta/);
+
+        if (isSelfClosing) {
+            const indent = ' '.repeat(INDENT_SIZE * indentLevel);
+            return indent + line;
+        }
+
+        const hasOpeningTag = strippedLine.match(/^<[a-z][a-z0-9]*(.*?)>.*$/) != null;
+        const hasClosingTag = strippedLine.match(/^.*<\/[a-z][a-z0-9]*(.*?)>$/)!= null;
+
+        if (hasClosingTag)  indentLevel -= 1;
+        const indent = ' '.repeat(INDENT_SIZE * indentLevel);
+        if (hasOpeningTag) indentLevel += 1;
+
+        return `${indent}${line}`;
+    }).join('\n');
+
+    return formatted;
 }
 
 createWikiFileStructure(MD_DIRECTORY);
