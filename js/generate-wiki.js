5e49e64cf6bd294c33c7df0ca3b3641773bd63e3
RickyTheRacc <rickytheracc@gmail.com>
2024-10-18 20:34:20 -0500

    Trolling?
    Who knows

--- js/generate-wiki.js
+++ js/generate-wiki.js
@@ -4,9 +4,9 @@ const { JSDOM } = require('jsdom');
 
 const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
 
-const WIKI_DIRECTORY = isGithubActions ? './wiki' : '../wiki';
-const MD_DIRECTORY = isGithubActions ? './md' : '../md';
-const WIKI_ENTRY_FILE = isGithubActions ? './html/index.html' : '../html/index.html';
+const WIKI_DIRECTORY = isGithubActions ? 'wiki' : '../wiki';
+const MD_DIRECTORY = isGithubActions ? 'md' : '../md';
+const WIKI_ENTRY_FILE = isGithubActions ? 'html/index.html' : '../html/index.html';
 const INDENT_SIZE = 4;
 
 const converter = new showdown.Converter({
